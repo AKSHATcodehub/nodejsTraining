4a7098b454cb4ab84e7e716658bb272761130106
Akshat Sahu <akshat.sahu@appinventiv.com>
2023-07-06 15:07:01 +0530

    updated code

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,18 +7,7 @@ export default class Routes{
 
    
     constructor(app:Application){
-        /**
-         * @openapi
-         * /sum
-         *  get:
-         *      tag:
-         *          - sum of two number
-         *          description:Responds if the app is up iouytrewand running
-         *          responses:
-         *              200:
-         *                  description:App is running
-         */
-        app.get("/sum",sumController.getSumData)
+        
         app.use('/api',allroutes)
     }
 }
